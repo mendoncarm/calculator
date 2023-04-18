be733a1882dc0d84d031479a3557e1fa74a6cbd4
Arthur Rosa Mendonça <mendoncaarm@ia.ita.br>
2023-04-18 16:35:52 -0300

    Calculadora

---
+++ java.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(event){
+   const ac = document.querySelector('#ac')
+   const maismenos = document.querySelector('#maismenos')
+   const percent = document.querySelector('#percent')
+});
